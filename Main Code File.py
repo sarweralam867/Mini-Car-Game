0efb0e1a3826b5b70e634d3fbfe13353e315f556
H.M. Sarwer Alam <86118255+sarweralam867@users.noreply.github.com>
2023-04-13 00:11:54 +0600

    Circle Obs done
    Circle Obs done

--- Main Code File.py
+++ Main Code File.py
@@ -16,24 +16,52 @@ def iterate():
 def colorSet(a,b,c):
     return glColor3f(a/255, b/255, c/255)
 
+def pixel(a):
+    return glPointSize(a)
+
 def showScreen():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     iterate()
-    colorSet(3, 236, 252) #konokichur color set (RGB)
+    157, 162, 171
     #call the draw methods here
 
     #circles
-    radius = 90
-    total_inside_circle = 20
-    originX = 600
-    originY = 600
-    #Circle.All_Circles(radius, total_inside_circle,originX,originY)
+    radius = 50
+    total_inside_circle = 100
+    d= 0
+    colorSet(156, 3, 11)
+    for i in range(2):
+        originX = 75
+        originY = 75 + d
+        Circle.All_Circles(radius, total_inside_circle,originX,originY)
+
+        originX = 75+150
+        originY = 75 + d + 150
+        Circle.All_Circles(radius, total_inside_circle, originX, originY)
+
+        originX = 75 + 300
+        originY = 75 + d + 300
+        Circle.All_Circles(radius, total_inside_circle,originX,originY)
+
+        originX = 75+450
+        originY = 75 + d + 450
+        Circle.All_Circles(radius, total_inside_circle, originX, originY)
+
+        d += 500
 
     # Lines
-    glPointSize(20)
+    pixel(20)
+    colorSet(157, 162, 171)
     MIDpoint.MidPoint(290,0,290,1000)
-    #MIDpoint.MidPoint(200,300,800,800)
+
+    displacement = 0
+    pixel(25)
+    colorSet(255, 255, 255)
+    for i in range(10):
+        MIDpoint.MidPoint(150, 0+displacement, 150, 50+displacement)
+        MIDpoint.MidPoint(450, 0 + displacement, 450, 50 + displacement)
+        displacement += 250
 
     glutSwapBuffers()
 
